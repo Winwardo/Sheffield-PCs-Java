8c940f019818ab89ee10c5731187b47db7e90b02
Topher Winward <winwardo@gmail.com>
2014-11-21 21:02:37 +0000

    Basic Building

---
+++ sheffield.pcs/src/main/java/data/Building.java
@@ -0,0 +1,77 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import databaseAccess.DatabasePostgres;
+
+public class Building {
+    public long id;
+    public int maximum;
+    public String name;
+    public String photo;
+    public double latitude;
+    public double longitude;
+
+    public static long insertNew(String name, int maximum, String photo,
+	    double latitude, double longitude) {
+	Connection connection = DatabasePostgres.getConnection();
+	PreparedStatement pStatement;
+	long result = -1l;
+
+	try {
+	    pStatement = connection
+		    .prepareStatement("INSERT INTO building (name, maximum, photo, latitude, longitude) "
+			    + "VALUES (?, ?, ?, ?, ?) RETURNING id");
+	    pStatement.setString(1, name);
+	    pStatement.setInt(2, maximum);
+	    pStatement.setString(3, photo);
+	    pStatement.setDouble(4, latitude);
+	    pStatement.setDouble(5, longitude);
+
+	    ResultSet newId = pStatement.executeQuery();
+
+	    if (newId.next()) {
+		result = newId.getLong("id");
+	    }
+
+	    newId.close();
+	    pStatement.close();
+	    connection.close();
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	return result;
+    }
+
+    public static Long findIdFromName(String buildingName) {
+	Connection connection = DatabasePostgres.getConnection();
+	PreparedStatement pStatement;
+	Long result = null;
+
+	try {
+	    pStatement = connection
+		    .prepareStatement("SELECT * FROM building WHERE name = ?");
+	    pStatement.setString(1, buildingName);
+
+	    ResultSet results = pStatement.executeQuery();
+
+	    if (results.next()) {
+		result = results.getLong("id");
+	    }
+
+	    results.close();
+	    pStatement.close();
+	    connection.close();
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	return null;
+    }
+}
