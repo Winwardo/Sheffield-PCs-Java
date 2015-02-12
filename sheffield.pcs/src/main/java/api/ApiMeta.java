6cdf9c9ed81c0567a3a01c5fc0bee6ea00ac5fd2
Topher Winward <winwardo@gmail.com>
2015-02-12 19:42:43 +0000

    Allowing untrusted SSL certs
    * This is a horrible hacky fix!

--- sheffield.pcs/src/main/java/api/ApiMeta.java
+++ sheffield.pcs/src/main/java/api/ApiMeta.java
@@ -1,7 +1,19 @@
 package api;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -10,11 +22,65 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.config.ConfigFactory;
 
+import databaseAccess.DatabaseLoginInfo;
+
 @Path("/meta")
 @Api(value = "/meta", description = "General info about the system, user")
 public class ApiMeta {
     static {
 	ConfigFactory.config().setBasePath("http://www.shefunipcs.info/api/");
+    }
+
+    @GET
+    @Path("/certs/{password}")
+    @ApiOperation(value = "/certs/{password}", notes = "Trust all SSL certs.")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String trustCerts(@PathParam("password") String password) {
+	// Hm. This is dubious at the least.
+	if (DatabaseLoginInfo.apiPassword.equals(password)) {
+	    // http://www.rgagnon.com/javadetails/java-fix-certificate-problem-in-HTTPS.html
+	    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+		@Override
+		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+		    return null;
+		}
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] certs,
+			String authType) throws CertificateException {
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] certs,
+			String authType) {
+		}
+
+	    } };
+
+	    SSLContext sc;
+	    try {
+		sc = SSLContext.getInstance("SSL");
+		sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc
+			.getSocketFactory());
+
+		// Create all-trusting host name verifier
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+		    @Override
+		    public boolean verify(String hostname, SSLSession session) {
+			return true;
+		    }
+		};
+		// Install the all-trusting host verifier
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+		return new Gson().toJson("Allows untrusted SSL certs.");
+	    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	}
+
+	return new Gson().toJson("Failed to allow untrusted SSL certs.");
     }
 
     @GET
