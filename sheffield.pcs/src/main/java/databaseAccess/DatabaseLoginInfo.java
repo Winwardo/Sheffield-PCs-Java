c359c4d5e2ede8848ee859a6b9e6ae2e52cae6ad
Topher Winward <winwardo@gmail.com>
2015-10-02 11:23:05 +0100

    Create DatabaseLoginInfo.java

---
+++ sheffield.pcs/src/main/java/databaseAccess/DatabaseLoginInfo.java
@@ -0,0 +1,11 @@
+package databaseAccess;
+
+// Dummy data
+public class DatabaseLoginInfo {
+    static String host = "";
+    static String dbName = ""
+    static String username = "";
+    static String password = "";
+
+    static public String apiPassword = "";
+}
